a91f0335c3f4fb67199b47b814d14f116029a21c
LeahTang <106444140+LeahTang@users.noreply.github.com>
2022-07-31 16:56:40 +0100

    Improved loading event

--- js/mypage.js
+++ js/mypage.js
@@ -1,11 +1,25 @@
+//
+//
+// document.addEventListener('DOMContentLoaded', function(){
+//   if (document.readyState == 'readyinteractive') {
+//     alert("page is ready");
+//   }
+//     document.getElementById("loader-index").style.display = "none";
+//     document.getElementById("loaded-index").style.display = "block";
+//     document.body.removeAttribute("style");
+//     document.body.style.color = "#fff";
+//     document.body.style.height = "auto";
+//     document.body.style.fontFamily = "'Cormorant Infant', serif";
+//     document.body.style.background = "black";
+//
+// });
 
-
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById("loader-index").style.display = "none";
-    document.getElementById("loaded-index").style.display = "block";
-    document.body.removeAttribute("style");
-    document.body.style.color = "#fff";
-    document.body.style.height = "auto";
-    document.body.style.fontFamily = "'Cormorant Infant', serif";
-    document.body.style.background = "black";
-});
+window.onload = function() {
+  document.getElementById("loader-index").style.display = "none";
+  document.getElementById("loaded-index").style.display = "block";
+  document.body.removeAttribute("style");
+  document.body.style.color = "#fff";
+  document.body.style.height = "auto";
+  document.body.style.fontFamily = "'Cormorant Infant', serif";
+  document.body.style.background = "black";
+}
