fec4f542b082c7d2b7e34302656a94bb47c476a3
LeahTang <106444140+LeahTang@users.noreply.github.com>
2022-07-31 16:23:10 +0100

    Added loading frame

---
+++ js/mypage.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("loader-index").style.display = "none";
+    document.getElementById("loaded-index").style.display = "block";
+    document.body.removeAttribute("style");
+    document.body.style.color = "#fff";
+    document.body.style.height = "auto";
+    document.body.style.fontFamily = "'Cormorant Infant', serif";
+    document.body.style.background = "black";
+});
