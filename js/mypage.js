f75d9d81bc44a13ca930043410ff045ebcae7a43
LeahTang <106444140+LeahTang@users.noreply.github.com>
2022-08-03 12:21:18 +0100

    Updated

--- js/mypage.js
+++ js/mypage.js
@@ -5,20 +5,51 @@ window.onload = setTimeout(quitLoader, 3000);
 
 
 function quitLoader() {
+  var language = "";
+  var page = "";
+  var pagename = "";
+  var pathname = document.location.pathname;
   var loader = document.getElementsByClassName("loader")[0];
   var loaded = document.getElementsByClassName("loaded")[0];
   var body = document.body;
   var uniBody = document.getElementsByClassName("uni-body")[0];
   var cnBody = document.getElementsByClassName("cn")[0];
 
+  for (let i =0; i < pathname.length; i++) {
+    if (pathname[i] == '/') {
+      page = "";
+    }
+    else {
+      page = page + pathname[i];
+    }
+  }
+
+  if (page.slice(0,2) == "cn") {
+    language = "cn";
+    pagename = page.slice(2,);
+  }
+  else {
+    pagename = page;
+  }
+
+
   loader.style.display = "none";
   loaded.style.display = "block";
   body.removeAttribute("style");
   body.style.color = "#fff";
   body.style.height = "auto";
-  body.style.fontFamily = "'Cormorant Infant', serif";
   body.style.background = "black";
-  uniBody.style.color = "#4a4a4a";
-  uniBody.style.backgroundColor = "white";
-  cnBody.style.fontFamily = "PMingLiU";
+
+  if (language == "cn"){
+    cnBody.style.fontFamily = "'Noto Serif SC', serif";
+  }
+  else {
+    body.style.fontFamily = "'Cormorant Infant', serif";
+  }
+
+  if (pagename == "projects-in-uni.html") {
+    uniBody.style.color = "#4a4a4a";
+    uniBody.style.backgroundColor = "white";
+  }
+  
 }
