398a152064f94aa169a8b2e49d945d26a88b3d4d
LeahTang <106444140+LeahTang@users.noreply.github.com>
2022-08-21 10:23:15 +0100

    Upgrade the details

--- js/mypage.js
+++ js/mypage.js
@@ -1,8 +1,9 @@
-
 /*Loader quiting*/
 
-window.onload = setTimeout(quitLoader, 5000);
+// window.onload = setTimeout(quitLoader, 5000);
 
+//Develoing mode
+window.onload = quitLoader;
 
 function quitLoader() {
   var language = "";
@@ -15,20 +16,18 @@ function quitLoader() {
   var uniBody = document.getElementsByClassName("uni-body")[0];
   var cnBody = document.getElementsByClassName("cn")[0];
 
-  for (let i =0; i < pathname.length; i++) {
+  for (let i = 0; i < pathname.length; i++) {
     if (pathname[i] == '/') {
       page = "";
-    }
-    else {
+    } else {
       page = page + pathname[i];
     }
   }
 
-  if (page.slice(0,2) == "cn") {
+  if (page.slice(0, 2) == "cn") {
     language = "cn";
-    pagename = page.slice(2,);
-  }
-  else {
+    pagename = page.slice(3, );
+  } else {
     pagename = page;
   }
 
@@ -40,16 +39,80 @@ function quitLoader() {
   body.style.height = "auto";
   body.style.background = "black";
 
-  if (language == "cn"){
+  if (language == "cn") {
     cnBody.style.fontFamily = "'Noto Serif SC', serif";
-  }
-  else {
+  } else {
     body.style.fontFamily = "'Cormorant Infant', serif";
   }
 
   if (pagename == "projects-in-uni.html") {
     uniBody.style.color = "#4a4a4a";
-    uniBody.style.backgroundColor = "white";
+    uniBody.style.backgroundColor = "#F2F2F2";
   }
-  
+
+}
+
+/* Narbar showing and hiding events
+  The events will be trigger by mouse wheel.
+*/
+
+document.onmousewheel = function() {
+
+  let navbarDisplay = document.getElementsByClassName("navbar-display")[0];
+  let navbar = document.getElementById("navbar");
+  let elContent = document.querySelector('.loaded').getBoundingClientRect();
+  let currPos = elContent.y;
+
+
+  if (document.getElementsByClassName("nav-animation")[0]) {
+    navbar.classList.remove("nav-animation");
+  }
+  navbar.classList.add("nav-animation");
+
+  let navAnimation = document.getElementsByClassName("nav-animation")[0];
+
+  if (event.wheelDelta < 0 && currPos < -100) {
+    navbarDisplay.style.display = "none";
+    navAnimation.style.animationName = "navbarHide";
+    navAnimation.style.animationDuration = "1s";
+    navAnimation.style.animationIterationCount = "1";
+  } else if (event.wheelDelta > 0) {
+
+    if (currPos > -100 || currPos == -100 || currPos == 0) {
+      navbarDisplay.style.display = "block";
+      navbar.classList.add("bg-transparent");
+
+    } else {
+      navbarDisplay.style.display = "block";
+      navAnimation.style.animationName = "navbarShow";
+      navAnimation.style.animationDuration = "1s";
+      navAnimation.style.animationIterationCount = "1";
+      navAnimation.style.animationDirection = "ease";
+      navbar.classList.remove("bg-transparent");
+      navbar.classList.add("bg-black");
+    }
+  }
+
+}
+
+
+// stage list tab onclick event
+function stageListOnclick(node){
+  node.classList.add('active');
+  for (const item of document.getElementsByClassName('stage-list-item')){
+    if (item.id !== node.id){
+      item.classList.remove('active');
+    }
+  }
+}
+
+// project classification onclick: direct to the projects
+function projectOnclick(link){
+  const node = document.getElementById(link);
+  node.classList.add('active');
+  for (const item of document.getElementsByClassName('stage-list-item')){
+    if (item.id !== node.id){
+      item.classList.remove('active');
+    }
+  }
 }
